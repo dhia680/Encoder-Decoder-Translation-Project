f10ba0c0f88ca14a66d88f362b9602581c9dc266
azzouginho <133368640+azzouginho@users.noreply.github.com>
2023-09-05 16:18:44 +0200

    Add files via upload

---
+++ hackathon-ponts/main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from utils import ask_question_to_pdf
+
+app = Flask(__name__)
+
+@app.route("/hello/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+from flask import render_template
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+from flask import request
+@app.route('/prompt', methods=['POST'])
+
+def prompt():
+    message = {}
+    data = request.form['prompt']
+    message['answer']= ask_question_to_pdf.ask_question_to_pdf(data)
+    return message
+
+#def prompt():
+    #message={}
+    #message ['answer'] = request.form['prompt'] + " double mooooonstre"
+    #return message

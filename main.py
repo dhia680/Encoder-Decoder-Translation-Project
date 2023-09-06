eb29010e4990a7eb6a9f2fff3639dee8b3c846a8
dhia680 <dhia.garbaya@eleves.enpc.fr>
2023-09-06 11:34:57 +0200

    ajout de commentaire

--- main.py
+++ main.py
@@ -3,32 +3,42 @@ from utils import ask_question_to_pdf
 
 app = Flask(__name__)
 
+
 @app.route("/hello/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 from flask import render_template
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 from flask import request
-@app.route('/prompt', methods=['POST'])
 
+
+@app.route("/prompt", methods=["POST"])
 def prompt():
     message = {}
-    data = request.form['prompt']
-    message['answer']= ask_question_to_pdf.gpt3_completion(data)
+    data = request.form["prompt"]
+    message["answer"] = ask_question_to_pdf.gpt3_completion(data)
     return message
+
 
 @app.route("/question", methods=["GET"])
 def question():
-    answer = ask_question_to_pdf.ask_question_to_pdf("Pose moi une question sur le texte")
-    return {"answer" : answer}
+    answer = ask_question_to_pdf.ask_question_to_pdf(
+        "Pose moi une question sur le texte"
+    )
+    return {"answer": answer}
 
-#def prompt():
-    #message={}
-    #message ['answer'] = request.form['prompt'] + " double mooooonstre"
-    #return message
+
+# def prompt():
+# message={}
+# message ['answer'] = request.form['prompt'] + " double mooooonstre"
+# return message
+
+# HELLO TEST DE PUSH

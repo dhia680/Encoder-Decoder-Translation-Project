c354b98716aba248a1d272cca74038b70b336dbb
dhia680 <dhia.garbaya@eleves.enpc.fr>
2023-09-07 16:15:32 +0200

    création de la base de donnée + fonction associée

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from flask import Flask
 from utils import ask_question_to_pdf
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
+
+
+# create the extension
+db = SQLAlchemy()
+# create the app
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ma_database.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+class ChatExchange(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.String(255))
+    assistant_response = db.Column(db.String(255))
+
+
+with app.app_context():
+    db.create_all()
+
+
+# from app.models import ChatExchange
+
+
+# Dans votre vue Flask pour afficher les échanges
+@app.route("/chat-history")  # Route pour afficher l'historique des échanges
+def show_chat_history():
+    chat_history = ChatExchange.query.all()
+    return render_template("index.html", chat_history=chat_history)
 
 
 q_list = []
@@ -14,15 +45,9 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-from flask import render_template
-
-
 @app.route("/")
 def hello():
     return render_template("index.html")  # , messages=messages)
-
-
-from flask import request
 
 
 @app.route("/prompt", methods=["POST"])
@@ -30,6 +55,10 @@ def prompt():
     message = {}
     data = request.form["prompt"]
     message["answer"] = ask_question_to_pdf.gpt3_completion(data)
+    # Enregistrez l'échange dans la base de données dès que la réponse d'OpenAI est générée
+    exchange = ChatExchange(user_message=data, assistant_response=message["answer"])
+    db.session.add(exchange)
+    db.session.commit()
     return message
 
 
